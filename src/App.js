3856cb126242aa555a5855959846eb920a546d58
essam omar <essam.93@hotmail.com>
2021-09-12 20:07:32 +0300

    add mab

--- src/App.js
+++ src/App.js
@@ -2,19 +2,67 @@ import React, { Component } from 'react';
 import Form from './component/Form';
 import Location from './component/Location';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 class App extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      display_name: "",
+      type: "",
+      lat: "",
+      lon: "",
+      showData: false,
+      
+    }
+  }
+  handleLocation = (e) => {
+    let display_name = e.target.value;
+    this.setState({
+      display_name: display_name
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+      let config = {
+        method: "GET",
+        baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
+      }
+      axios(config).then(res => {
+        let responseData = res.data[0]
+        this.setState({
+          display_name: responseData.display_name,
+          lon: responseData.lon,
+          lat: responseData.lat,
+          showData: true,
+          
+        })
+
+      })
+    
+   
+  }
 
   render() {
     return (
       <div>
-        <Form  />
-        
-         
-          <Location 
+
+        <Form handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
+
+        {
+          this.state.showData &&
+          <Location display_name={this.state.display_name}
+            lat={this.state.lat}
+            lon={this.state.lon}
+            err={this.state.error}
           />
-       
+
+        }
+     
+
 
       </div>
     )
--- src/component/Location.js
+++ src/component/Location.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 class Location extends Component {
     render() {
         return (
@@ -7,6 +8,7 @@ class Location extends Component {
                 <h1>{this.props.display_name}</h1>
                 <h3>{this.props.lat}</h3>
                 <h3>{this.props.lon}</h3>
+                <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center= ${this.props.lat},${this.props.lon}&zoom=1-18`} alt=""></img>
             </div>
         )
     }
